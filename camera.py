58ecbda4c06d6d45a15de07f19f95a46056ae2cb
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-05-24 23:15:09 +0530

    init

---
+++ camera.py
@@ -0,0 +1,26 @@
+import cv2
+
+# Open the default camera
+cap = cv2.VideoCapture(0)
+
+# Check if the camera is opened successfully
+if not cap.isOpened():
+    print("Failed to open camera")
+else:
+    # Read and display frames from the camera
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Failed to read frame")
+            break
+
+        cv2.imshow("Camera", frame)
+
+        # Exit if 'q' is pressed
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # Release the camera
+    cap.release()
+    cv2.destroyAllWindows()
