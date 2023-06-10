894d999384190df5f0b853aad8a60b183f41b279
MeGaTroNOO7 <bernardgeorgecharles007@gmail.com>
2023-06-10 23:24:22 +0530

    init

--- emotions.py
+++ emotions.py
@@ -111,7 +111,7 @@ elif mode == "display":
     # start the webcam feed
     cap = cv2.VideoCapture(0)
     if cap != True:
-        print("Working at accurracy : 65.6%") 
+        print() 
     while True:
         # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
